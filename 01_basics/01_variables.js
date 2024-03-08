1a7207cb5d18ca16933577ee67735850f47b59b9
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-08 09:38:57 +0000

    conti

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "vineet@gmail.com"
+var accoutPassword = "12345"
+accountCity = "Rewari"
+
+//accountId = 2 // not allowed in javascript
+
+accountEmail = "Yash@gmail.com"
+accoutPassword = "11212"
+accountCity = "jaipur"
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail, accountId, accoutPassword, accountCity, accountState])
+
+/*
+Prefer not to use var keyword
+because of issue in block scope and funcitonal scope
+*/
