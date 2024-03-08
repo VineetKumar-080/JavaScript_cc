1327fc0936716b521e9b2081a6a6bdeb0d18b4b5
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-08 10:21:45 +0000

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as newer version
+
+//alert (3 + 3) // we are using node.js, not browser
+
+// code readablity should be high
+console.log(3 // this is not a good code to read
+    +3)        
+
+console.log("vineet")
+
+let name = "vineet" // this is a string datatype
+let age = 21 // number datatype
+let isLoggedIn = false// boolean datatype
+
+// PRIMITIVE DATATYPES
+//number => 2 to the power 53
+// bigint => for big integer
+// string => "" use double quotes for string datatype
+// boolean => true/false
+// null(it a datatype and as well as it a stand alone value)
+/* undefined => it is used when something is not defined properly,
+ we also use null to perfectly say that the this is empty.*/
+//symbol => to find the uniqueness in the code
+
+
+//object
+
+console.log(typeof undefined); //output - undefined
+console.log(typeof null); // output - object
+//we use typeof to find the type of the value
+
+/* Null is an object but Undefined is type. we can say
+that undefined is datatype in itself*/
