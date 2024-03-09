4c21207c9aff3c375e9b795d953f89c7babe6192
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-09 11:44:13 +0000

    learn about conv.and operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = undefined
-console.log(typeof score); 
-console.log(typeof(score));
+// console.log(typeof score); 
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 /*
 "33" => 33
@@ -18,9 +18,37 @@ true => 1; false => 0
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+//******************Operations******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2^3 
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Vineet"
+let str3 = str1+str2
+//console.log(str3);
+
+// console.log("1" + 2); 
+// console.log(1+ "2");
+// console.log("1" + 2 + 2); //agr string first hai to sabhi ko string me convert kiya jata hai
+// console.log(1 + 2 + "2"); // agr string last me hai to phle wale sb ka conversion ho jaega
+
+let gameCounter = 100
+++gameCounter; //precedence operator prefix and postfix
+console.log(gameCounter);
