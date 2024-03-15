a66b72503851cbd68f3ac637a504e030d684a8cc
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-15 12:25:04 +0000

    Learn about while and do-while loop in JS

---
+++ 05_iterations/02_while_do_loop.js
@@ -0,0 +1,18 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index ${index}`);
+//     index = index+2
+// }
+
+// let myArr = ['flash','superman','shaktiman']
+// let arr = 0
+// while (arr<myArr.length) {
+//     console.log(`value is ${myArr[arr]}`);
+//     arr = arr + 1
+// }
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
