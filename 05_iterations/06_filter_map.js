892c0aba6f8293c8680bfca6b2b6ae2ddc8713b9
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-17 07:49:26 +0000

    Learn about filters in js

---
+++ 05_iterations/06_filter_map.js
@@ -0,0 +1,26 @@
+// const coding = ["js", "ruby", "java" , "python", "capp"]
+// const values = coding.forEach((item) => {
+//     // console.log(item);
+//     return item
+
+// })
+// console.log(values);
+
+// With the help of filter we can get the filtered values or we can say 
+// we get the values who pass the condition which we mention in the filter method()
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newNums  = myNums.filter( (num) => {
+//      return num > 4   // if we start the curly braces then we have to write the return function 
+// })  // but if we write the code like {(num) => num > 4}, then we don't have to write the return function 
+
+
+// example of if we want to use foreach loop instead of filter.
+// const newNums = []
+// myNums.forEach( (num)=> {
+//     if(num > 4){
+//         newNums.push(num)
+//     }
+// })
+// console.log(newNums);
+
+
