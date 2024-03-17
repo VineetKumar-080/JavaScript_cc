afedeb83475032a08e2f9dc1ac157dd276a8db4c
VineetKumar-080 <162236246+VineetKumar-080@users.noreply.github.com>
2024-03-17 07:50:04 +0000

    Learn about reduce function in js

---
+++ 05_iterations/08_reduce.js
@@ -0,0 +1,34 @@
+const myNums = [1,2,3]
+// const  myTotal = myNums.reduce(function (accumulator, currentvalue) {
+//     console.log(`accumulator: ${accumulator} and currentValue: ${currentvalue}`);
+//     return accumulator + currentvalue
+// },0)
+
+
+//+++++++ same but with arrow function
+
+const myTotal = myNums.reduce( (accumulator, currentValue) => accumulator+currentValue, 0)
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price : 3000,
+    },
+    {
+        itemName: "python course",
+        price : 999,
+    },
+    {
+        itemName: "mobile dev course",
+        price : 7000,
+    },
+    {
+        itemName: "data sciece course",
+        price : 12000,
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (accumulator,item) => accumulator + item.price,0)
+ console.log(priceToPay);
+
